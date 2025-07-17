773d1a653aba195c434b9431df2dbc26cc128481
consultorialendario <consultor.ia.lendario@gmail.com>
2025-07-17 15:23:02 -0300

    Update main.jsx
    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Google Analytics (opcional)
-if (import.meta.env.VITE_GA_TRACKING_ID) {
-  const script = document.createElement('script')
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GA_TRACKING_ID}`
-  script.async = true
-  document.head.appendChild(script)
-
-  window.dataLayer = window.dataLayer || []
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date())
-  gtag('config', import.meta.env.VITE_GA_TRACKING_ID)
-  window.gtag = gtag
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
